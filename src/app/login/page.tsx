66e17b1d4a749d154a60a3e12cb8b81673dddb10
Kursat Korkmaz <kursat.korkmaz123@hotmail.com>
2025-08-13 07:23:32 +0300

    SignUp form is started to implement.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,7 @@
 "use client";
 import SignInForm from "@/components/login/signin-form";
 import SignUpForm from "@/components/login/signup-form";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { FormEvent, useState } from "react";
 
 export default function AuthenticationPage() {
@@ -28,8 +25,20 @@ export default function AuthenticationPage() {
       <div className="w-full max-w-sm md:max-w-3xl">
         <div className="flex flex-col gap-6">
           <Card className="overflow-hidden p-0">
-            <CardContent className="relative grid p-0  md:grid-cols-2">
-              {inSignInMode ? <SignInForm /> : <SignUpForm />}
+            <CardContent className="relative grid p-0 md:grid-cols-2">
+              {inSignInMode ? (
+                <SignInForm
+                  dontHaveAccountHandler={() => {
+                    setMode("signup");
+                  }}
+                />
+              ) : (
+                <SignUpForm
+                  createAccountHandler={() => {
+                    setMode("signin");
+                  }}
+                />
+              )}
 
               <div className="bg-muted relative hidden md:block">
                 <img
@@ -37,15 +46,13 @@ export default function AuthenticationPage() {
                   alt="Image"
                   className="absolute inset-0 h-full w-full object-cover blur-[2px]"
                 />
-                <div className="z-1 absolute top-0 left-0 w-full h-full bg-radial from-transparent from-50% to-black to-90% ">
-                  a
-                </div>
+                <div className="z-1 absolute top-0 left-0 w-full h-full bg-radial from-transparent from-50% to-black to-90% "></div>
               </div>
             </CardContent>
           </Card>
 
           <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-            By clicking continue, you agree to our{" "}
+            By clicking continue, you agree to our&nbsp;
             <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.
           </div>
         </div>
--- src/components/login/signup-form.tsx
+++ src/components/login/signup-form.tsx
@@ -1,61 +1,167 @@
+import { betterAuthClient } from "@/lib/better-auth/auth-client";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import z4 from "zod/v4";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+
+const RegisterSchenma = z4.object({
+  name: z4.string().min(1, "Name is required"),
+  email: z4.email("Invalid email address"),
+  password: z4.string().min(8, "Password must be at least 8 characters long"),
+});
 
 type SignUpFormProps = {
   signUpHandler?: (e: React.FormEvent<HTMLFormElement>) => void;
   createAccountHandler?: () => void;
 };
 export default function SignUpForm(props: SignUpFormProps) {
+  const formHook = useForm<z4.infer<typeof RegisterSchenma>>({
+    resolver: zodResolver(RegisterSchenma),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const signUpHandler = () => {
+    betterAuthClient.signUp.email({
+      email: "",
+      name: "",
+      password: "",
+      callbackURL: "/",
+      image: "",
+      lang: "",
+      fetchOptions: {
+        onSuccess(context) {},
+        onError(context) {},
+      },
+    });
+  };
+
   return (
-    <form className="p-6 md:p-8" noValidate onSubmit={props.signUpHandler}>
-      <div className="flex flex-col gap-6">
-        <div className="flex flex-col items-center text-center">
-          <h1 className="text-2xl font-bold">Welcome back</h1>
+    <div className="p-6 md:p-8 flex flex-col gap-6">
+      <div className="flex flex-col items-center text-center">
+        <h1 className="text-2xl font-bold">Welcome back</h1>
 
-          <p className="text-muted-foreground text-balance">
-            Sign up from Hepsify Platform
-          </p>
-        </div>
-        <div className="grid gap-3">
-          <Label htmlFor="email">Email</Label>
-          <Input
-            id="email"
-            type="email"
-            name="email"
-            autoComplete="email"
-            placeholder="m@example.com"
-            required
-          />
-        </div>
-        <div className="grid gap-3">
-          <div className="flex items-center">
-            <Label htmlFor="password">Password</Label>
-          </div>
-          <Input
-            id="password"
-            type="password"
-            name="password"
-            autoComplete="current-password"
-            required
-          />
-        </div>
-        <Button type="submit" className="w-full">
-          Sign up
-        </Button>
-
-        <div className="text-center text-sm">
-          Do you have an account?
-          <Button
-            className="w-min px-1"
-            variant={"link"}
-            type="button"
-            onClick={props.createAccountHandler}
-          >
-            Sign in
-          </Button>
-        </div>
+        <p className="text-muted-foreground text-balance">
+          Sign up for Hepsify Platform
+        </p>
       </div>
-    </form>
+      <Form {...formHook}>
+        <form noValidate onSubmit={props.signUpHandler}>
+          <div className="flex flex-col gap-3">
+            <FormField
+              control={formHook.control}
+              name="name"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>
+                      <span className="text-red-500">*</span>Name
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="text"
+                        autoComplete="name"
+                        placeholder="John Doe"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={formHook.control}
+              name="email"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>
+                      <span className="text-red-500">*</span>Email
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="email"
+                        autoComplete="email"
+                        placeholder="m@example.com"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={formHook.control}
+              name="password"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>
+                      <span className="text-red-500">*</span>Email
+                    </FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        autoComplete="new-password"
+                        placeholder="****"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <div className="grid gap-3">
+              <div className="flex items-center">
+                <Label htmlFor="password">
+                  <span className="text-red-500">*</span>Password
+                </Label>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                name="password"
+                autoComplete="current-password"
+                placeholder="********"
+                required
+              />
+            </div>
+          </div>
+
+          <Button type="submit" className="w-full">
+            Sign up
+          </Button>
+
+          <div className="text-center text-sm">
+            Do you have an account?
+            <Button
+              className="w-min px-1"
+              variant={"link"}
+              type="button"
+              onClick={props.createAccountHandler}
+            >
+              Sign in
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 }
